e9ed85a20b3cf0e82adfa4ecbb2841657cd0c7eb
Isak Karlsson <isak-kar@dsv.su.se>
2013-04-22 17:50:13 +0200

    fix: updated experimenter script

--- bin/rrexp.sh
+++ bin/rrexp.sh
@@ -2,9 +2,11 @@
 FOLDER="$1"
 OUTPUT="$2"
 ITERATIONS="$3"
+EX="$4"
 
 echo "** Start experiment ($ITERATIONS iterations) **"
 echo "Input folder: $FOLDER"
+echo "Experiment: $EX"
 echo "Output file: $OUTPUT (will be REMOVED)"
 read -p "Are you sure? (y/N) " -n 1
 echo
@@ -13,12 +15,13 @@ case $REPLY in
 	rm "$OUTPUT"
 	for f in $FOLDER/*.txt; do  
 	    for ((i=1; i<=$ITERATIONS; i++)); do
-		echo "** Iteration $i ** "
-		echo -n "Iteration $i," >> "$OUTPUT";
-		./rr -i "$f" -o csv "$@" >> "$OUTPUT";
+		echo "info: running $f iteration $i/$ITERATIONS"
+		TMP=`./rr -i "$f" -o csv "$@" 2>$ /dev/null`
+		while read -r line; do
+		    echo "Iteration $i, $EX, $line" >> "$OUTPUT"
+		done  <<< "$TMP";
 	    done
 	done;;
 	*)
 	exit 1;;
 esac
-
