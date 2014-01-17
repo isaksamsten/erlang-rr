c8798f7740592a0c14e61875c6a45efa073b1a16
Isak Karlsson <isak-kar@dsv.su.se>
2014-01-17 09:01:41 +0100

    fix: add rr_processor

---
+++ src/rr_processor.erl
@@ -0,0 +1,26 @@
+%%% @author Isak Karlsson <isak-kar@dsv.su.se>
+%%% @copyright (C) 2013, Isak Karlsson
+%%% @doc
+%%%
+%%% @end
+%%% Created : 17 Jan 2013 by Isak Karlsson <isak-kar@dsv.su.se>
+
+-module(rr_processor).
+-export([
+         behaviour_info/1,
+         find/1
+        ]).
+
+behaviour_info(callbacks) ->
+    [{evaluate, 2}].
+
+%% @doc find a processor given a string
+find(EString) ->
+    case rr:get_processor(EString) of
+        {Processor, Args} ->
+            Opts = Processor:args(Args),
+            Processor:new(Opts);
+        error ->
+            throw({module_not_found, EString})
+    end.
+
